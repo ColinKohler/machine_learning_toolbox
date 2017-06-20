685543a7edd7b0fa28ff52223e1e3c293ef9a8ec
ColinKohler <colink78@gmail.com>
2017-06-20 17:35:25 -0400

    Renamed utils to respect tensorflow and added various tf layer constructors to it.

---
+++ utils/tf_utils.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+import numpy as np
+
+# Load the class encoding
+def loadClassEncoding(path):
+    one_hot_encoding = dict()
+    with open(path, 'r') as f:
+        for line in f:
+            cls, num = line.split(' ', 1)
+            one_hot_encoding[cls] = int(num)
+    return one_hot_encoding
+
+# Create Convolutional layer, to split computation use groups > 1
+def conv(x, filter_height, filter_width, num_filters, stride_y, stride_x, name, padding='SAME', groups=1):
+    input_channels = int(x.get_shape()[-1])
+    convolve = lambda i, k: tf.nn.conv2d(i, k, strides=[1, stride_y, stride_x, 1], padding=padding)
+
+    with tf.variable_scope(name) as scope:
+        weights = tf.get_variable('weights', shape=[filter_height, filter_width, input_channels/groups, num_filters])
+        biases = tf.get_variable('biases', shape=[num_filters])
+
+        # Handle splitting of conv layers onto 2 GPUs
+        if groups == 1:
+            conv = convolve(x, weights)
+        else:
+            input_groups = tf.split(axis=3, num_or_size_splits=groups, value=x)
+            weight_groups = tf.split(axis=3, num_or_size_splits=groups, value=weights)
+            output_groups = [convolve(i, k) for i,k in zip(input_groups,weight_groups)]
+
+            conv = tf.concat(axis=3, values=output_groups)
+
+        bias = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape().as_list())
+        relu = tf.nn.relu(bias, name=scope.name)
+
+        return weights, biases, relu
+
+# Create Fully-connected Layer
+def fc(x, num_in, num_out, name ,relu=True):
+    with tf.variable_scope(name) as scope:
+        weights = tf.get_variable('weights', shape=[num_in, num_out], trainable=True)
+        biases = tf.get_variable('biases', shape=[num_out], trainable=True)
+
+        act = tf.nn.xw_plus_b(x, weights, biases, name=scope.name)
+        relu = tf.nn.relu(act) if relu else act
+
+        return weights, biases, relu
+
+# Create max pooling layer
+def max_pool(x, filter_height, filter_width, stride_y, stride_x, name, padding='SAME'):
+    return tf.nn.max_pool(x, ksize=[1, filter_height, filter_width, 1],
+                          strides=[1, stride_y, stride_x, 1], padding=padding, name=name)
+
+# Create local response normalization layer
+def lrn(x, radius, alpha, beta, name, bias=1.0):
+    return tf.nn.local_response_normalization(x, depth_radius=radius, alpha=alpha, beta=beta, bias=bias, name=name)
+
+# Create dropout layer
+def dropout(x, keep_prob):
+    return tf.nn.dropout(x, keep_prob)
