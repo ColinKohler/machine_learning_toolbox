87f4419d776d4031efd20fa638b8af1ca8b38bd1
ColinKohler <colink78@gmail.com>
2017-06-28 14:27:49 -0400

    Changed full conv alexnet to work on arbiraty image sizes to get more/less detection cells.

--- scripts/alexnet/run_fully_conv_alexnet_model.py
+++ scripts/alexnet/run_fully_conv_alexnet_model.py
@@ -11,16 +11,18 @@ from tensorflow_networks.alexnet import Alexnet
 import utils.tf_utils as tf_utils
 
 def run(args):
+    img_size = 579
     img = cv2.imread(args.image_path)
-    img = cv2.resize(img, (451, 451))
     rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    height, width, channels = img.shape
+    img = cv2.resize(img, (img_size, img_size))
     img = img.astype(np.float32)
 
     one_hot_encoding = tf_utils.loadClassEncoding(args.class_encoding_path)
     num_classes = len(one_hot_encoding)
 
     # TensorFlow stuff
-    x = tf.placeholder(tf.float32, [1, 451, 451, 3])
+    x = tf.placeholder(tf.float32, [1, img_size, img_size, 3])
 
     # Init model and connect output to last layer
     model = Alexnet(x, num_classes, full_conv=True)
@@ -33,15 +35,23 @@ def run(args):
         sess.run(tf.global_variables_initializer())
         saver.restore(sess, args.model_path)
 
-        best = sess.run(model.output, feed_dict={x : img.reshape([1, 451, 451, 3]), model.keep_prob : 1.0})
-        #print best[0].argmax(axis=2)
-        best_cell = np.unravel_index(best[0,:,:,1].argmax(), [8,8])
+        cells = (img_size - 227) / 32 + 1
+        best = sess.run(model.output, feed_dict={x : img.reshape([1, img_size, img_size, 3]), model.keep_prob : 1.0})
+        best_cell = np.unravel_index(best[0,:,:,1].argmax(), [cells,cells])
         min_point = (best_cell[1] * 32, best_cell[0] * 32)
         print best_cell
 
         fig, ax = plt.subplots(1)
         ax.imshow(rgb_img)
-        rect = patches.Rectangle(min_point, 227, 227, linewidth=1, edgecolor='r', facecolor='none')
+        sx = width / float(img_size)
+        sy = height / float(img_size)
+        p = (int(min_point[0]*sy), int(min_point[1]*sx))
+        lx = int(227 * sx)
+        ly = int(227 * sy)
+        print p
+        print lx
+        print ly
+        rect = patches.Rectangle(p, ly, lx, linewidth=1, edgecolor='r', facecolor='none')
         ax.add_patch(rect)
         plt.show()
 
